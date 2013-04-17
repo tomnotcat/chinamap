1efcf9698e70f9c24ddee95ad73230ca59c3e668
tomnotcat <tomnotcat@tomnotcat-desktop.(none)>
2013-04-17 18:57:24 +0800

    Add tianjing, shanghai, jiangxi

--- convert.sh
+++ convert.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 files=`ls -1`
 for i in $files; do
-    if [ "$i" != "output" ] && [ "$i" != "convert.sh" ]; then
+    if [ "$i" != "output" ] && [ "$i" != "convert.sh" ] && \
+        [ "$i" != "china-old.svg" ] && [ "$i" != "china-new.svg" ] \
+        && [ "$i" != "svg2swf-0.5.win32.exe.zip" ]; then
         svg2swf "$i" "output/${i%.*}.swf"
     fi
 done
