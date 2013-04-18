fa4247f35f86c2e9d1d4217b6c5b961d2f32e954
tomnotcat <tomnotcat@tomnotcat-tinysoft.(none)>
2013-04-18 14:11:40 +0800

    Update bkgnd.svg

--- convert.sh
+++ convert.sh
@@ -3,7 +3,7 @@ files=`ls -1`
 for i in $files; do
     if [ "$i" != "output" ] && [ "$i" != "convert.sh" ] && \
         [ "$i" != "china-old.svg" ] && [ "$i" != "china-new.svg" ] \
-        && [ "$i" != "svg2swf-0.5.win32.exe.zip" ]; then
+        && [ "$i" != "README.md" ] && [ "$i" != "svg2swf-0.5.win32.exe.zip" ]; then
         svg2swf "$i" "output/${i%.*}.swf"
     fi
 done
